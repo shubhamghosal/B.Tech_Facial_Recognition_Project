33664d18590a022a5131fd160001a12f6712a27c
Shubham Ghosal <33888982+shubhamghosal@users.noreply.github.com>
2021-06-17 00:38:13 +0530

    Add files via upload

---
+++ create_data.py
@@ -0,0 +1,63 @@
+# OpenCV Facial Recognition
+# Designed by Shubham Ghosal
+
+# script used to create a dataset of someone's face
+
+import cv2, numpy, sys, os, time
+# change the paths below to the location where these files are on your machine
+haar_file = '/Users/Asus/Desktop/face-recognition-master/haarcascade_frontalface_default.xml'
+# All of the faces data (images) will be stored here
+datasets = '/Users/Asus/Desktop/face-recognition-master/faces'
+# Sub dataset in 'faces' folder. Each folder is specific to an individual person
+# change the name below when creating a new dataset for a new person
+sub_dataset = 'Shubham'
+
+# join the paths to include the sub_dataset folder
+path = os.path.join(datasets, sub_dataset)
+# if sub_dataset folder doesn't already exist, make the folder with the name defined above
+if not os.path.isdir(path):
+    os.mkdir(path)
+
+# defining the size of images
+(width, height) = (130, 100)
+
+face_cascade = cv2.CascadeClassifier(haar_file)
+# use '0' for internal (built-in) webcam or '1' for external ones
+webcam = cv2.VideoCapture(0)
+# returns true or false (if the camera is on or not)
+print("Webcam is open? ", webcam.isOpened())
+# wait for the camera to turn on (just to be safe, in case the camera needs time to load up)
+time.sleep(2)
+#Takes pictures of detected face and saves them
+count = 1
+print("Taking pictures...")
+# this takes 100 pictures of your face. Change this number if you want.
+# Having too many images, however, might slow down the program
+while count < 101:
+    # im = camera stream
+    ret_val, im = webcam.read()
+    # if it recieves something from the webcam...
+    if ret_val == True:
+        gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+        # detect face using the haar cascade file
+        faces = face_cascade.detectMultiScale(gray, 1.3, 4)
+        for (x,y,w,h) in faces:
+            # draws a rectangle around your face when taking pictures
+            # this is to create a ROI (region of interest) so it only takes pictures of your face
+            cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,0),2)
+            # define 'face' as the inside of the rectangle we made above and make it grayscale
+            face = gray[y:y + h, x:x + w]
+            # resize the face images to the size of the 'face' variable above (i.e: area captured inside of the rectangle)
+            face_resize = cv2.resize(face, (width, height))
+            # save images with their corresponding number
+            cv2.imwrite('%s/%s.png' % (path,count), face_resize)
+        count += 1
+        # display the openCV window
+        cv2.imshow('OpenCV', im)
+        key = cv2.waitKey(20)
+        # press esc to stop the loop
+        if key == 27:
+            break
+print("Sub dataset for your face has been created.")
+webcam.release()
+cv2.destroyAllWindows()
---
+++ face_recognize.py
@@ -0,0 +1,71 @@
+# OpenCV Facial Recognition
+# Reorganized by Shubham Ghosal
+
+# face_recognize.py
+# script used to: train a model on people's faces and recognize the faces using the client-side webcam stream
+
+size = 4
+import cv2, sys, numpy, os
+
+# change the paths below to the location where these files are on your machine
+# haar_file path
+haar_file = '/Users/Asus/Desktop/face-recognition-master/haarcascade_frontalface_default.xml'
+# path to the main faces directory which contains all the sub_datasets
+datasets = '/Users/Asus/Desktop/face-recognition-master/faces'
+
+print('Training classifier...')
+# Create a list of images and a list of corresponding names along with a unique id
+(images, labels, names, id) = ([], [], {}, 0)
+for (subdirs, dirs, files) in os.walk(datasets):
+    for subdir in dirs:
+        # the person's name is the name of the sub_dataset created using the create_data.py file
+        names[id] = subdir
+        subjectpath = os.path.join(datasets, subdir)
+        for filename in os.listdir(subjectpath):
+            path = subjectpath + '/' + filename
+            label = id
+            images.append(cv2.imread(path, 0))
+            labels.append(int(label))
+        id += 1
+(width, height) = (130, 100)
+
+# Create a numpy array from the lists above
+(images, labels) = [numpy.array(lists) for lists in [images, labels]]
+
+# OpenCV trains a model from the images using the FisherFace algorithm
+model = cv2.face.FisherFaceRecognizer_create()
+# train the FisherFaces algorithm on the images and labels we provided above
+model.train(images, labels)
+# use fisherRecognizer on webcam stream
+face_cascade = cv2.CascadeClassifier(haar_file)
+webcam = cv2.VideoCapture(0)
+print('Classifier trained!')
+print('Attempting to recognize faces...')
+while True:
+    (_, im) = webcam.read()
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    # detect faces using the haar_cacade file
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        # colour = bgr format
+        # draw a rectangle around the face and resizing/ grayscaling it
+        # uses the same method as in the create_data.py file
+        cv2.rectangle(im,(x,y),(x + w,y + h),(0, 255, 255),2)
+        face = gray[y:y + h, x:x + w]
+        face_resize = cv2.resize(face, (width, height))
+        # try to recognize the face(s) using the resized faces we made above
+        prediction = model.predict(face_resize)
+        cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 255), 2)
+        # if face is recognized, display the corresponding name
+        if prediction[1]<500:
+               cv2.putText(im,'%s' % (names[prediction[0]]),(x + 10, (y + 22) + h), cv2.FONT_HERSHEY_PLAIN,1.5,(20,205,20), 2)
+        # if face is unknown (if classifier is not trained on this face), show 'Unknown' text...
+        else:
+          cv2.putText(im,'Unknown',(x + 10, (y + 22) + h), cv2.FONT_HERSHEY_PLAIN,1.5,(65,65, 255), 2)
+
+    # show window and set the window title
+    cv2.imshow('OpenCV Face Recognition -  esc to close', im)
+    key = cv2.waitKey(10)
+    # esc to quit applet
+    if key == 27:
+        break
